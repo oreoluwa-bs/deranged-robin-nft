b37f9f5fe10e13be5c7420040513fa5b8e091b03
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-02-19 12:52:24 +0100

    :wrench: tiny fix

--- app/(dashboard)/create/page.tsx
+++ app/(dashboard)/create/page.tsx
@@ -1,6 +1,23 @@
 import { HiArrowRight } from "react-icons/hi2";
 
+const imageSizes = [
+  "128",
+  "256",
+  "384",
+  "448",
+  "512",
+  "576",
+  "640",
+  "704",
+  "768",
+  "832",
+  "896",
+  "960",
+  "1024",
+];
+
 export default function Create() {
+  function s() {}
   return (
     <main className="min-h-[calc(100vh-170px)]">
       <section className="main-container">
@@ -18,8 +35,65 @@ export default function Create() {
             </div>
             <div className="max-w-sdm">
               <form>
-                <div>
-                  <textarea className="min-h-[150px] w-full rounded-lg bg-slate-50/10" />
+                <div className="grid gap-2">
+                  <div>
+                    <label
+                      htmlFor="prompt"
+                      className="mb-1 inline-block text-sm"
+                    >
+                      Prompt
+                    </label>
+                    <textarea
+                      name="prompt"
+                      id="prompt"
+                      className="min-h-[150px] w-full rounded-lg bg-slate-50/10 px-2 py-2"
+                      placeholder="Enter a prompt to create original, realistic images and art from a text description"
+                    />
+                  </div>
+                  <div className="grid grid-cols-2 gap-4">
+                    <div>
+                      <label
+                        htmlFor="width"
+                        className="mb-1 inline-block text-sm"
+                      >
+                        Width
+                      </label>
+                      <select
+                        name="width"
+                        id="width"
+                        defaultValue={imageSizes[4]}
+                        className="w-full rounded-lg bg-slate-50/10 px-2 py-2"
+                      >
+                        {imageSizes.map((size) => (
+                          <option key={size} value={size}>
+                            {size}
+                          </option>
+                        ))}
+                      </select>
+                      {/* Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits */}
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="height"
+                        className="mb-1 inline-block text-sm"
+                      >
+                        Height
+                      </label>
+                      <select
+                        name="height"
+                        id="height"
+                        defaultValue={imageSizes[4]}
+                        className="w-full rounded-lg bg-slate-50/10 px-2 py-2"
+                      >
+                        {imageSizes.map((size) => (
+                          <option key={size} value={size}>
+                            {size}
+                          </option>
+                        ))}
+                      </select>
+                      {/* Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits */}
+                    </div>
+                  </div>
                 </div>
                 <button className="mt-6 inline-flex items-center justify-center gap-2 rounded-lg bg-gray-700 px-4 py-2 text-white">
                   Generate
