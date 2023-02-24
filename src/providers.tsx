e4f754116baa6585efb473dcd543aa884f36907e
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-02-24 15:52:14 +0100

    :wrench: switch to goerli

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import { WagmiConfig, createClient, configureChains } from "wagmi";
-import { hardhat, goerli } from "wagmi/chains";
+import { mainnet, goerli } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { InjectedConnector } from "wagmi/connectors/injected";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+// import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [goerli],
+  [process.env.NODE_ENV !== "production" ? goerli : mainnet],
   [
     publicProvider(),
-    // alchemyProvider({ apiKey: process.env.ALCHEMY_GOERLI_API_KEY! }),
+    alchemyProvider({
+      apiKey:
+        process.env.NODE_ENV !== "production"
+          ? process.env.ALCHEMY_GOERLI_API_KEY!
+          : process.env.ALCHEMY_API_KEY!,
+    }),
   ]
 );
 
