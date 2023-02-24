05f206a6a1c8afa8af0c44f506981cf9f54ca392
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-02-24 20:39:20 +0100

    :wrench: convert fetcher to async

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,9 +1,8 @@
-export const fetcher = (...args: Parameters<typeof fetch>) => {
-  return fetch(...args).then(async (res) => {
-    const response = await res.json();
-    if (!res.ok) {
-      throw new Error(response.message ?? "Something went wrong");
-    }
-    return response;
-  });
+export const fetcher = async (...args: Parameters<typeof fetch>) => {
+  const res = await fetch(...args);
+  const response = await res.json();
+  if (!res.ok) {
+    throw new Error(response.message ?? "Something went wrong");
+  }
+  return await response;
 };
