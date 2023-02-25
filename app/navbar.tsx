5b44661ce43d3bd1f3eacdac5a75ccd483b8f535
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-02-25 23:58:46 +0100

    :wrench: tiny

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+// import { Rammetto_One } from "@next/font/google";
 import Link from "next/link";
 import ConnectWalletButton from "~/components/connect-wallet-button";
+
+// const permanentMarker = Rammetto_One({
+//   subsets: ["latin"],
+//   weight: "400",
+//   variable: "--font-marker",
+// });
 
 export default function Navbar() {
   return (
@@ -14,7 +21,9 @@ export default function Navbar() {
             height={48}
             priority
           />
-          <span className="hidden md:inline-block">DERANGED ROBIN NFT</span>
+          <span className={`hidden text-base md:inline-block `}>
+            DERANGED ROBIN NFT
+          </span>
         </Link>
 
         <div className="flex items-center gap-5 lg:gap-10">
